27b1227bc1d10d456934f8fbe4e15acc94a7ec65
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-10-23 11:49:59 +0100

    linting

--- src/main.py
+++ src/main.py
@@ -10,14 +10,14 @@ import requests
 
 s3_client = boto3.client("s3")
 
-SOURCE_BUCKET = os.environ.get('S3_SOURCE_BUCKET')
-SOURCE_KEY = os.environ.get('S3_SOURCE_KEY')
-DESTINATION_BUCKET = os.environ.get('S3_DESTINATION_BUCKET')
-DESTINATION_KEY = os.environ.get('S3_DESTINATION_KEY')
-LLM_GATEWAY_URL = os.environ.get('SECRET_LLM_GATEWAY_URL')
+SOURCE_BUCKET = os.environ.get("S3_SOURCE_BUCKET")
+SOURCE_KEY = os.environ.get("S3_SOURCE_KEY")
+DESTINATION_BUCKET = os.environ.get("S3_DESTINATION_BUCKET")
+DESTINATION_KEY = os.environ.get("S3_DESTINATION_KEY")
+LLM_GATEWAY_URL = os.environ.get("SECRET_LLM_GATEWAY_URL")
 
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
 
 def call_llm_gateway(text):
@@ -50,9 +50,9 @@ def log_timing(func):
 
 @log_timing
 def read_csv_from_s3():
-    s3 = boto3.client('s3')
+    s3 = boto3.client("s3")
     obj = s3.get_object(Bucket=SOURCE_BUCKET, Key=SOURCE_KEY)
-    df = pd.read_csv(obj['Body'])
+    df = pd.read_csv(obj["Body"])
     return df
 
 
@@ -60,16 +60,16 @@ def read_csv_from_s3():
 def transform_data(df):
     # Use a call to the gateway to examine each line in the input-data.csv file and correct any spelling mistakes.
     for index, row in df.iterrows():
-        text = row['text']
+        text = row["text"]
     # Call LLM Gateway API to transform text
     transformed_text = call_llm_gateway(text)
-    df.at[index, 'text'] = transformed_text
+    df.at[index, "text"] = transformed_text
     logging.debug("Row %s transformed at %s", index, datetime.now().isoformat())
     return df
 
 
 @log_timing
 def write_csv_to_s3(df):
-    s3 = boto3.client('s3')
+    s3 = boto3.client("s3")
     csv_buffer = df.to_csv(index=False)
     s3.put_object(Bucket=DESTINATION_BUCKET, Key=DESTINATION_KEY, Body=csv_buffer)
